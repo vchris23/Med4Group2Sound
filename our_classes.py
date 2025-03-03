4dd9117545893d8e9c3146a9e999cc5e7ffbd830
Victor Løfsted Christiansen <vchris23@student.aau.dk>
2025-03-03 12:35:24 +0100

    Track class defined

---
+++ our_classes.py
@@ -0,0 +1,13 @@
+﻿class Track:
+    def __init__(self, sound, label):
+        self.sound = sound
+        self.label:str = label
+        self.source:str = ""
+        self.train_or_test:str = ""
+        self.original_track:str = ""
+        self.features:list = []
+
+#Tracks = [Track(1,"hello"), Track(2, "I am god"), Track("Me", "Behave")]
+
+#labels = [track.label for track in Tracks]
+#print(labels)
