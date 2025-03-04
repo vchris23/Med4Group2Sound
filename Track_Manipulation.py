88b2b12e2d13cc06d03f4a5afa4a2754db40bc2f
Josephine1000j <148771304+Josephine1000j@users.noreply.github.com>
2025-03-04 13:24:26 +0100

    first

---
+++ Track_Manipulation.py
@@ -0,0 +1,12 @@
+﻿
+
+
+new_tracks=[] #makes new list to store new tracks
+
+for track in tracks:
+    sound = track["sound"]  #gets the sound from tracks
+
+for start in range(0, duration, chunk_size):
+    excerpt = sound[start:start + chunk_size]  # Lav et udsnit af lyden
+
+
