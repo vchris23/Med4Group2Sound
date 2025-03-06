f8bc84c83e0fecf75b99dc51ebd70c2451f6a2d6
Josephine1000j <148771304+Josephine1000j@users.noreply.github.com>
2025-03-06 08:19:16 +0100

    Update Track_Manipulation.py

--- Track_Manipulation.py
+++ Track_Manipulation.py
@@ -4,14 +4,17 @@ def split_tracks_into_excerpts(tracks):
     new_tracks = []  #laver en ny liste, til de nye tracks
 
     for track in tracks:
-        sound = track["sound"]  #lyden bliver hentet, fra tracket
+        #sound, sr = librosa.load(track["sound"], sr=None)
+        #chunk_size = sr * 15
+
+        sound = track["sound"] #lyden bliver hentet, fra tracket
         chunk_size = 15  #størrelsen af hver chunk sættes
 
         #lyder bliver delt op i chunks af 15 sek
         for start in range(0, len(sound), chunk_size):
-            excerpt = sound[start:start + chunk_size]  #laver en bid af lyden
+            excerpt = sound[start:start + chunk_size]  #laver en bid af lyden på 15 sek
             new_track = track.copy()  #laver en kopi af tracket
-            new_track["sound"] = excerpt  #opdater den kopierede track, med den nye lyd
+            new_track["sound"] = excerpt  #opdaterer den kopierede track, med den nye lyd
             new_tracks.append(new_track)  #tilføjer det nye track til listen (fra toppen)
 
     return new_tracks  #returner listen med alle de nye tracks
