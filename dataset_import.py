17c56289a8e8a13f3930f6d169d59165e950164f
etomikadj01 <148781565+etomikadj01@users.noreply.github.com>
2025-04-15 09:36:24 +0200

    dataset

---
+++ dataset_import.py
@@ -0,0 +1,9 @@
+﻿from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+categories_list = np.loadtxt('Dataset/categories.txt', dtype = str, delimiter="@")
+clusters_list = np.loadtxt('Dataset/clusters.txt', dtype = str, delimiter="@")
+
+audio_files = [f for f in listdir('Dataset/Audio') if isfile(join('Dataset/Audio', f))]
+print(audio_files)
